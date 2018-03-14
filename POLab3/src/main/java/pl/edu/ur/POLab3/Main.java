74ad69150989161d426eebe85fb457616f8324b2
Your Name <you@example.com>
2018-03-14 17:37:31 +0100

    Zadania Lab

--- POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
+++ POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
@@ -18,6 +18,57 @@ public class Main {
         // TODO 1.4, 1.7, 
         // TODO 2.2
         // TODO 3.3, 3.4
+       
+        
+        System.out.println(silnia(8));
+        System.out.println(silniaPentla(8));
+    }
+
+    public static void zadanie1_3() {
+        int a = 6;
+        int b = 4;
+
+        int c = a & b;
+
+        int d = a | b;
+
+        System.out.println(c); // 4
+        System.out.println(d); // 6
+        if (a != 0 && b == 4) {
+            System.out.println("Obie wartosci sa prawdziwe");
+        }
+    }
+
+    public static void zadanie1_6() {
+        int a = 5;
+        int b = 3;
+        //int c = a + b++;
+        int d = (a++) + b;
+        //System.out.println(c);
+        System.out.println(d);
+    }
+
+    public static int silnia(int wartosc) {
+
+        if (wartosc == 0) {
+            return 1;
+        } else {
+            return wartosc * silnia(wartosc - 1);
+        }
     }
     
+    public static int silniaPentla(int wartosc){
+        int wynik=1;
+        for (int i = 1; i <= wartosc; i++) {
+            wynik*=i;
+        }
+        return wynik;
+    }
+    
+    public static int Fibb(int numerLiczby)
+    {
+        if (numerLiczby == 1 || numerLiczby == 2) {
+            return 1;
+        }
+        return 0;
 }
